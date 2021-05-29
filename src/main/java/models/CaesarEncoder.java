d0469fe7e49de923412f12233a30aef6ddf0ba09
Serah wakaba <serahwakaba9@gmail.com>
2021-05-29 22:24:44 +0300

    encoder getter and setter commit

--- src/main/java/models/CaesarEncoder.java
+++ src/main/java/models/CaesarEncoder.java
@@ -1,4 +1,36 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CaesarEncoder {
+    private String userInput;
+    private int shiftKey;
+    private List<String> userOutput = new ArrayList<String>();
+
+    public String getUserInput() {
+        return userInput;
+    }
+
+    public void setUserInput(String userInput) {
+        this.userInput = userInput;
+    }
+
+    public int getShiftKey() {
+        return shiftKey;
+    }
+
+    public void setShiftKey(int shiftKey) {
+        this.shiftKey = shiftKey;
+    }
+
+    public List<String> getUserOutput() {
+        return userOutput;
+    }
+
+    public void setUserOutput(List<String> userOutput) {
+        this.userOutput = userOutput;
+    }
+
+
 }
