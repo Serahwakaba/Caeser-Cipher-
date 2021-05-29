7120c29fc2e5245e783e19b2e2fad5ad94942fe9
Serah wakaba <serahwakaba9@gmail.com>
2021-05-29 23:12:48 +0300

    import models commit

---
+++ src/main/java/App.java
@@ -0,0 +1,9 @@
+import models.CaesarDecoder;
+import models.CaesarEncoder;
+
+public class App {
+
+    CaesarEncoder encoder = new CaesarEncoder("", 0);
+    CaesarDecoder decoder = new CaesarDecoder("", 0);
+
+}
