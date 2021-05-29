d88ed0a31ad2a0da2b1a6b1846160f7863f6154f
Serah wakaba <serahwakaba9@gmail.com>
2021-05-29 23:14:27 +0300

    buffed reader commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,14 @@
 import models.CaesarDecoder;
 import models.CaesarEncoder;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class App {
+
+
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    boolean programRunning = true;
 
     CaesarEncoder encoder = new CaesarEncoder("", 0);
     CaesarDecoder decoder = new CaesarDecoder("", 0);
