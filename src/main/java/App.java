317d494885410703350995963fae1ed4d7af2ba7
Serah wakaba <serahwakaba9@gmail.com>
2021-05-30 10:22:53 +0300

    import decode and encode classes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,10 @@
-//import models.CaesarEncoder;
-//import models.CaesarDecoder;
-//
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
-//
+import models.CaesarEncoder;
+import models.CaesarDecoder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 //public class App {
 //    public static void main(String[] args) throws IOException {
 //
