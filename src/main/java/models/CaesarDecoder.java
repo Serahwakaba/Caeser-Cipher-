0fe51a93fa8ffd8582e9baacf8e691aa79e439bd
Serah wakaba <serahwakaba9@gmail.com>
2021-05-30 10:07:02 +0300

    private methods added

--- src/main/java/models/CaesarDecoder.java
+++ src/main/java/models/CaesarDecoder.java
@@ -1,48 +1,60 @@
-package models;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class CaesarDecoder {
-
-    private CaesarDecoder EncoderDetails = new CaesarDecoder("", 0);
-
-    public CaesarDecoder getEncoderDetails() {
-        return EncoderDetails;
-    }
-
-    public void setEncoderDetails(CaesarDecoder encoderDetails) {
-        EncoderDetails = encoderDetails;
-    }
-
-    public String getUserText() {
-        return userText;
-    }
-
-    public void setUserText(String userText) {
-        this.userText = userText;
-    }
-
-    public int getDecodeKey() {
-        return decodeKey;
-    }
-
-    public void setDecodeKey(int decodeKey) {
-        this.decodeKey = decodeKey;
-    }
-
-    public List<String> getDecodedOutput() {
-        return decodedOutput;
-    }
-
-    public void setDecodedOutput(List<String> decodedOutput) {
-        this.decodedOutput = decodedOutput;
-    }
-
-    private String userText;
-    private int decodeKey;
-    private List<String> decodedOutput = new ArrayList<String>();
-
-    public CaesarDecoder(String s, int i) {
-    }
-}
+//package models;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class CaesarDecoder {
+//    private CaesarEncoder EncoderDetails = new CaesarEncoder("", 0);
+//
+//    private String userText;
+//    private int decodeKey;
+//    private List<String> decodedOutput = new ArrayList<String>();
+//
+//
+//    public CaesarDecoder(String userText, int decodeKey) {
+//        this.userText = userText;
+//        this.decodeKey = decodeKey;
+//    }
+//
+//    public String decodeUserText() {
+//        for (int i = 0; i < userText.length(); i++) {
+//            if (Character.isUpperCase(userText.charAt(i))) {
+//                int decodedValue = ((int)(userText.charAt(i)) + (26 - decodeKey) - 65) % 26 + 65;
+//                char decodedChar = (char)(decodedValue);
+//                String decodedString = Character.toString(decodedChar);
+//                decodedOutput.add(decodedString);
+//            } else if (Character.isLowerCase(userText.charAt(i))) {
+//                int decodedValue = ((int)(userText.charAt(i)) + (26 - decodeKey) - 97) % 26 + 97;
+//                char decodedChar = (char)(decodedValue);
+//                String decodedString = Character.toString(decodedChar);
+//                decodedOutput.add(decodedString);
+//            } else {
+//                String encodedString = Character.toString(userText.charAt(i));
+//                decodedOutput.add(encodedString);
+//            }
+//        }
+//        return String.join("", decodedOutput);
+//    }
+//
+//    // Getter methods
+//    public String getUserText() {
+//        this.userText = EncoderDetails.getInputString();
+//        return this.userText;
+//    }
+//
+//    public int getUserKey() {
+//        this.decodeKey = EncoderDetails.getShiftKey();
+//        return this.decodeKey;
+//    }
+//
+//    // The two functions below are setter functions used to set user input from App.java as
+//    // properties of an instance of this class when the program is running. Use these setter
+//    // functions instead of the constructor to set the properties
+//    public String setUserText(String userInput) {
+//        return this.userText = userInput;
+//    }
+//
+//    public int setUserKey(int userKey) {
+//        return this.decodeKey = userKey;
+//    }
+//}
